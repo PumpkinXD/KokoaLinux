54625ac6b86f28062c47babf23e2ce9b73d54650
PumpkinXD <1042588515@qq.com>
2022-04-23 00:13:42 +0800

    20220425
    replaced File().getAbsolutePath() with Paths.get().toAbsolutePath().toString()
    
    os && arch detect in next commit(s)
    
    (maybe 1.7.10 support in future? lol)

--- src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
+++ src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
@@ -2,11 +2,20 @@ package cn.yesterday17.kokoalinux.input;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+//import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public interface InputNative extends Library {
+/*
     InputNative instance = (InputNative)Native.loadLibrary(
         (new File("./native/libkokoa.so")).getAbsolutePath(), 
         InputNative.class);
+*/
+InputNative instance = (InputNative)Native.loadLibrary(
+        Paths.get("./native/libkokoa.so").toAbsolutePath().toString(), 
+        InputNative.class);
+
 
     /////////////////////////////////////
 
