a989b3ed6b4f8601f6f1de024292e9e053984440
PumpkinXD <1042588515@qq.com>
2022-05-12 18:01:32 +0800

    an unfinished simple native lib loader (**** u jna 3,4)

--- src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
+++ src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
@@ -2,19 +2,15 @@ package cn.yesterday17.kokoalinux.input;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-//import java.io.File;
+import com.sun.jna.Platform;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.commons.lang3.SystemUtils;
 
 public interface InputNative extends Library {
-/*
-    InputNative instance = (InputNative)Native.loadLibrary(
-        (new File("./native/libkokoa.so")).getAbsolutePath(), 
-        InputNative.class);
-*/
-InputNative instance = (InputNative)Native.loadLibrary(
-        Paths.get("./native/libkokoa.so").toAbsolutePath().toString(), 
-        InputNative.class);
+
+
+    InputNative instance =  InputNativeLoader.load();
 
 
     /////////////////////////////////////
@@ -40,4 +36,30 @@ InputNative instance = (InputNative)Native.loadLibrary(
     void prepareLocale();
 
     void setDebug(long debug);
+}
+
+/**
+ * InputNativeLoader
+ */
+ class InputNativeLoader {
+    static InputNative load(){
+        //todo: extract native lib to ./config/kokoalinux/os-arch
+        String libpath; 
+        os=SystemUtils.OS_NAME.toLowerCase();
+        if(Platform.isIntel())
+        {
+            if(Platform.is64Bit())
+            {arch="x86-64";}
+            else
+            {arch="x86";}
+        }
+        else
+        arch=SystemUtils.OS_ARCH;
+        
+        libpath=Paths.get("./config/kokoalinux/libkokoa",os + "-" + arch,"libkokoa.so").toAbsolutePath().toString();
+        return (InputNative)Native.loadLibrary(libpath,InputNative.class);
+    }
+    static String arch;
+    static String os;
+    
 }
