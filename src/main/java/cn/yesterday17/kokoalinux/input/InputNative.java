98b65f2358320c0bd85415f8289444e666cad7a1
PumpkinXD <1042588515@qq.com>
2022-07-14 10:16:43 +0800

    extract the lib to a temp dir(WIP)

--- src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
+++ src/main/java/cn/yesterday17/kokoalinux/input/InputNative.java
@@ -43,7 +43,7 @@ public interface InputNative extends Library {
  */
  class InputNativeLoader {
     static InputNative load(){
-        //todo: extract native lib to ./config/kokoalinux/libkokoa/os-arch/
+        //todo: ~~extract native lib to ./config/kokoalinux/libkokoa/os-arch/~~ gonna deprecate this method, f*** jna3.4
         String libpath; 
         os=SystemUtils.OS_NAME.toLowerCase();
         if(Platform.isIntel())
@@ -59,6 +59,47 @@ public interface InputNative extends Library {
         libpath=Paths.get("./config/kokoalinux/libkokoa",os + "-" + arch,"libkokoa.so").toAbsolutePath().toString();
         return (InputNative)Native.loadLibrary(libpath,InputNative.class);
     }
+    static InputNative LoadFromJar() throws IOException {//https://github.com/adamheinrich/native-utils
+    if(tempLibFileDir==null)
+        {
+           tempLibFileDir = createTempDir();
+           tempLibFileDir.deleteOnExit();
+        }
+        File tempLib=new File (tempLibFileDir,"libkokoa.so");
+
+        try (InputStream is=InputNativeLoader.class.getResourceAsStream(/*path to so, with plat dect*/)){
+            Files.copy(is, tempLib.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            
+        } catch (IOException e) {
+            tempLib.delete();
+            throw e;
+            //TODO: handle exception
+        }
+        catch (NullPointerException e) {
+            tempLib.delete();
+        }
+        InputNative inputnative_inst;
+        try {
+            inputnative_inst=(Native)Native.loadLibrary(tempLib.getAbsolutePath(),InputNative.class);
+        }         
+        finally {
+            if(SystemUtils.IS_OS_UNIX){tempLib.delete();}else{tempLib.deleteOnExit();}
+        }
+            return inputnative_inst;
+        }
+
+    static File createTempDir() throws IOException {
+        File genDir = new File(SystemUtils.getJavaIoTmpDir(),"kokoa"+System.nanoTime());
+        if(!genDir.mkdir()){
+            throw new IOException("Failed to create temp dir" + genDir.getName());
+        }
+        return genDir;
+
+    }
+
+
+
+    static File tempLibFileDir;
     static String arch;
     static String os;
     
